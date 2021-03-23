8cb0541fcd74c77d5bcd24ebf3a7dc76cd066e70
Enrique Ortuno <enrique@ortuno.net>
2021-03-22 21:00:25 -0400

    interface basis

---
+++ 02-typescript-project/06.property-indexing.js
@@ -0,0 +1,25 @@
+const paolo = {
+  name: "Paolo",
+  city: "Siena",
+  age: 44,
+};
+
+console.log(paolo["city"]);
+
+const key = "city";
+console.log(paolo[key]);
+
+const tom = {
+  name: "Tom",
+  city: "Munich",
+  age: 33,
+};
+
+function filterPerson(arr, term, key) {
+  return arr.filter(function (person) {
+    return person[key].match(term);
+  });
+}
+
+var result = filterPerson([paolo, tom], "Siena", "city");
+console.log(result);
