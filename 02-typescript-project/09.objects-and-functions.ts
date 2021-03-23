c4e8175ca0a9cb4fe27f4c023ba18e2eea904325
Enrique Ortuno <enrique@ortuno.net>
2021-03-22 21:51:06 -0400

    objects, interfaces and functions

---
+++ 02-typescript-project/09.objects-and-functions.ts
@@ -0,0 +1,8 @@
+// const tom = {
+//   name: "Tom",
+//   city: "Munich",
+//   age: 33,
+//   printDetails: function () {
+//     console.log(`${this.name} - ${this.city}`);
+//   },
+// };
---
+++ 02-typescript-project/10.objects--interfaces.ts
@@ -0,0 +1,14 @@
+// interface IPerson {
+//   name: string;
+//   city: string;
+//   age: number;
+// }
+
+// const tom: IPerson = {
+//   name: "Tom",
+//   city: "Munich",
+//   age: 33,
+//   printDetails: function () {
+//     console.log(`${this.name} - ${this.city}`);
+//   },
+// };
