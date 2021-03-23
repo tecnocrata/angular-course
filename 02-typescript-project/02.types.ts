d51fc4f94c90da2e1606d3886b48c1131504fd13
Enrique Ortuno <enrique@ortuno.net>
2021-03-22 20:42:44 -0400

    simple examples

---
+++ 02-typescript-project/02.types.ts
@@ -0,0 +1,7 @@
+// var greeting = "Hello John";
+// var age = 33;
+// var aBoolean = false;
+// console.log(typeof aBoolean); // "boolean"
+
+// aBoolean = "Tom";
+// console.log(typeof aBoolean); // "string"
---
+++ 02-typescript-project/03.explicit-arrays.ts
@@ -0,0 +1,10 @@
+// function filterByTerm(input: Array<string>, searchTerm: string) {
+//   if (!searchTerm) throw Error("searchTerm cannot be empty");
+//   if (!input.length) throw Error("input cannot be empty");
+//   const regex = new RegExp(searchTerm, "i");
+//   return input.filter(function (arrayElement) {
+//     return arrayElement.url.match(regex);
+//   });
+// }
+
+// filterByTerm(["string1", "string2", "string3"], "java");
