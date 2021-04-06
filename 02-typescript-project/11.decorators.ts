16561a906ec5af1191907c519c41cb443457e61b
Enrique Ortuno <enrique@ortuno.net>
2021-04-05 21:19:46 -0400

    angular basics generated

--- 02-typescript-project/11.decorators.ts
+++ 02-typescript-project/11.decorators.ts
@@ -1,13 +1,18 @@
-// const addFuelToRocket = (target: typeof Rocket) => {
-//   return class extends target {
-//     fuel = 100;
-//   };
-// };
+const addFuelToRocket = (target: typeof Rocket) => {
+  return class extends target {
+    fuel = 100;
+    destiny = "moon";
+  };
+};
 
-// // @addFuelToRocket
-// class Rocket {
-//   constructor(public fuel: number = 0) {}
-// }
+@addFuelToRocket
+class Rocket {
+  constructor(public fuel: number = 0) {}
+}
 
-// const rocket = new Rocket();
-// console.log(rocket.fuel);
+let rocket = new Rocket();
+console.log(rocket.fuel);
+// const key: string = "destiny";
+// console.log(rocket[key]);
+console.log(Object.getOwnPropertyNames(rocket));
+console.log(Object.entries(rocket));
---
+++ angular-cli.md
@@ -0,0 +1,13 @@
+## Creating an APP
+
+### Generic way
+
+```
+ng new angular-blog
+```
+
+### Using specific angular version
+
+```
+npx @angular/cli@11 new angular-blog
+```
