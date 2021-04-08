a68c48deeaf73b0deb98dfe61a66db96359f7ffe
Enrique Ortuno <enrique@ortuno.net>
2021-04-07 22:01:05 -0400

    interpolation and binding

--- 04-angular-basics/src/app/app.component.ts
+++ 04-angular-basics/src/app/app.component.ts
@@ -3,8 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-basics';
+  firstName = 'Enrique';
+  selectedValue = '';
+  animal = {
+    name: 'Lion',
+    image: './assets/images/lion.jpg',
+  };
+  htmlContent = 'The name is Bond <b>James Bond<b/>';
+  scriptContent = "<script>console.log('fire')</script>";
+  theme = {
+    color: 'indianred',
+    fontSize: '15px',
+    opacity: 0.7,
+  };
+  expression = '';
+  result = '0';
+
+  public showName() {
+    //console.log(this.firstName);
+    this.firstName = 'Carlos';
+  }
+
+  convertToDate() {
+    const date = new Date();
+    return date.toLocaleDateString();
+  }
+
+  calculate() {
+    this.result = eval(this.expression);
+  }
 }
