9b949e2c2c0cadc3f8908684c71e7172b0386fe0
Enrique Ortuno <enrique@ortuno.net>
2021-04-07 22:13:58 -0400

    standard directives

--- 04-angular-basics/src/app/app.component.ts
+++ 04-angular-basics/src/app/app.component.ts
@@ -23,6 +23,30 @@ export class AppComponent {
   expression = '';
   result = '0';
 
+  lugares: any = [
+    { nombre: 'Florería la gardenia' },
+    { nombre: 'Donas la pasadita' },
+    { nombre: 'Veterinaria huellas' },
+  ];
+
+  personas: any = [
+    { edad: 10, nombre: 'Suri' },
+    { edad: 28, nombre: 'Mónica' },
+    { edad: 18, nombre: 'Jennifer' },
+    { edad: 20, nombre: 'Shirley' },
+    { edad: 1, nombre: 'Bebé' },
+    { edad: 18, nombre: 'Juan' },
+  ];
+
+  historial: any = [
+    { estatura: 1.5, porte: 'pequeño', edad: 18, nombre: 'Kevin' },
+    { estatura: 1.8, porte: 'mediano', edad: 15, nombre: 'Lui' },
+    { estatura: 2.0, porte: 'alto', edad: 21, nombre: 'Karla' },
+    { estatura: 1.7, porte: 'mediano', edad: 14, nombre: 'Roman' },
+    { estatura: 2.2, porte: 'alto', edad: 25, nombre: 'Juan' },
+    { estatura: 1.4, porte: 'pequeño', edad: 28, nombre: 'pedro' },
+  ];
+
   public showName() {
     //console.log(this.firstName);
     this.firstName = 'Carlos';
