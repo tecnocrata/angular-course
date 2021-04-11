56ceff7c8e9924fe3bf4ebc841ade6140dbed28b
Enrique Ortuno <enrique@ortuno.net>
2021-04-11 13:25:26 -0400

    Basic main structure

--- 05-tailwindcss-material/tailwind.config.js
+++ 05-tailwindcss-material/tailwind.config.js
@@ -1,11 +1,22 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    // Some useful comment
+    extend: {
+      colors: {
+        primary: "#3b7977",
+        secondary: "#57a99a",
+      },
+      height: {
+        128: "20rem",
+        half: "50vh",
+        "3/4": "75vh",
+      },
+    },
+    variants: {
+      // Some useful comment
+    },
+    plugins: [
+      // Some useful comment
+    ],
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+};
